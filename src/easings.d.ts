8abc9f25b7738dcf0f5110fe6ea77d27f61f3764
Theo <finchasaurus@gmail.com>
2025-06-06 19:19:49 -0600

    easings types

---
+++ src/easings.d.ts
@@ -0,0 +1,12 @@
+type EasingFunction = (t: number) => number;
+
+export declare const linear: EasingFunction;
+export declare const cubic_in: EasingFunction;
+export declare const cubic_out: EasingFunction;
+export declare const cubic_in_out: EasingFunction;
+export declare const bounce_in: EasingFunction;
+export declare const bounce_out: EasingFunction;
+export declare const bounce_in_out: EasingFunction;
+export declare const elastic_in: EasingFunction;
+export declare const elastic_out: EasingFunction;
+export declare const elastic_in_out: EasingFunction;
